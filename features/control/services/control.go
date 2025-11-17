d5511b2a27484074b87e41acaef56e53470f8f3d
Rizal Anggoro <gnoogler4@gmail.com>
2025-11-17 11:14:39 +0700

    feat: implement door control methods with MQTT publishing

--- features/control/services/control.go
+++ features/control/services/control.go
@@ -1,6 +1,12 @@
 package services
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"time"
+
+	"github.com/Smart-Door-Lock-IoT/api/features/control/dto/responses"
+	httputils "github.com/Smart-Door-Lock-IoT/api/pkg/http/utils"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 type Control struct {
 	mqttClient *mqtt.Client
@@ -12,6 +18,49 @@ func NewControl(mqttClient *mqtt.Client) *Control {
 	}
 }
 
-func (s *Control) TriggerOpenDoor()        {}
-func (s *Control) TriggerFingerprintMode() {}
-func (s *Control) TriggerRFIDMode()        {}
+func (s *Control) TriggerOpenDoor() (*responses.TriggerOpenDoorResponse, *httputils.Error) {
+	if token := (*s.mqttClient).Publish(
+		"smart-door-lock-iot/open-door",
+		2,
+		false,
+		"triggered",
+	); token.WaitTimeout(time.Second*5) && token.Error() != nil {
+		return nil, httputils.NewInternalError(token.Error())
+	} else {
+		return &responses.TriggerOpenDoorResponse{
+			Message: "oke",
+		}, nil
+	}
+}
+
+func (s *Control) TriggerFingerprintMode() (
+	*responses.TriggerFingerprintModeResponse, *httputils.Error,
+) {
+	if token := (*s.mqttClient).Publish(
+		"smart-door-lock-iot/fingerprint-mode",
+		2,
+		false,
+		"triggered",
+	); token.WaitTimeout(time.Second*5) && token.Error() != nil {
+		return nil, httputils.NewInternalError(token.Error())
+	} else {
+		return &responses.TriggerFingerprintModeResponse{
+			Message: "oke",
+		}, nil
+	}
+}
+
+func (s *Control) TriggerRFIDMode() (*responses.TriggerRFIDModeResponse, *httputils.Error) {
+	if token := (*s.mqttClient).Publish(
+		"smart-door-lock-iot/rfid-mode",
+		2,
+		false,
+		"triggered",
+	); token.WaitTimeout(time.Second*5) && token.Error() != nil {
+		return nil, httputils.NewInternalError(token.Error())
+	} else {
+		return &responses.TriggerRFIDModeResponse{
+			Message: "oke",
+		}, nil
+	}
+}
