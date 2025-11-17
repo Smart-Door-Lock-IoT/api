9a004eed1d2820722569db9d3fd07a4a9a6da522
Rizal Anggoro <gnoogler4@gmail.com>
2025-11-17 11:11:05 +0700

    feat: add response types and handlers for door control operations

---
+++ features/control/dto/responses/control.go
@@ -0,0 +1,13 @@
+package responses
+
+type TriggerOpenDoorResponse struct {
+	Message string `json:"message"`
+} // @name TriggerOpenDoorResponse
+
+type TriggerFingerprintModeResponse struct {
+	Message string `json:"message"`
+} // @name TriggerFingerprintModeResponse
+
+type TriggerRFIDModeResponse struct {
+	Message string `json:"message"`
+} // @name TriggerRFIDModeResponse
--- features/control/handlers/control.go
+++ features/control/handlers/control.go
@@ -1,22 +1,67 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Smart-Door-Lock-IoT/api/features/control/services"
+	httpresponses "github.com/Smart-Door-Lock-IoT/api/pkg/http/responses"
+	"github.com/gin-gonic/gin"
+)
 
 type Control struct {
+	service *services.Control
 }
 
-func NewControl() *Control {
-	return &Control{}
+func NewControl(service *services.Control) *Control {
+	return &Control{
+		service: service,
+	}
 }
 
+// @id 			TriggerOpenDoor
+// @tags 		Control
+// @success 	200 {object} responses.TriggerOpenDoorResponse
+// @router 		/api/v1/control/open-door [post]
 func (h *Control) TriggerOpenDoor(c *gin.Context) {
-
+	if res, err := h.service.TriggerOpenDoor(); err != nil {
+		c.AbortWithStatusJSON(
+			err.Code, httpresponses.Error{
+				Message: err.Message,
+			},
+		)
+	} else {
+		c.JSON(http.StatusOK, res)
+	}
 }
 
+// @id 			TriggerFingerprintMode
+// @tags 		Control
+// @success 	200 {object} responses.TriggerFingerprintModeResponse
+// @router 		/api/v1/control/fingerprint-mode [post]
 func (h *Control) TriggerFingerprintMode(c *gin.Context) {
-
+	if res, err := h.service.TriggerFingerprintMode(); err != nil {
+		c.AbortWithStatusJSON(
+			err.Code, httpresponses.Error{
+				Message: err.Message,
+			},
+		)
+	} else {
+		c.JSON(http.StatusOK, res)
+	}
 }
 
+// @id 			TriggerRFIDMode
+// @tags 		Control
+// @success 	200 {object} responses.TriggerRFIDModeResponse
+// @router 		/api/v1/control/rfid-mode [post]
 func (h *Control) TriggerRFIDMode(c *gin.Context) {
-
+	if res, err := h.service.TriggerRFIDMode(); err != nil {
+		c.AbortWithStatusJSON(
+			err.Code, httpresponses.Error{
+				Message: err.Message,
+			},
+		)
+	} else {
+		c.JSON(http.StatusOK, res)
+	}
 }
