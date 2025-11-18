7cec89242704218424fe728c46f19b73fcc2ee5f
Rizal Anggoro <gnoogler4@gmail.com>
2025-11-18 15:36:55 +0700

    feat: implement log activity subscription with JSON handling and logging functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 	"time"
 
+	"github.com/Smart-Door-Lock-IoT/api/features/control/dto/requests"
+	"github.com/Smart-Door-Lock-IoT/api/injector"
 	"github.com/Smart-Door-Lock-IoT/api/pkg/http"
 	"github.com/Smart-Door-Lock-IoT/api/pkg/mqttclient"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -27,12 +30,19 @@ func main() {
 	go func() {
 		client := mqttclient.New()
 
+		handlers := injector.InitControlHandlers()
+
 		// subscribe log activities
 		if token := (*client).Subscribe(
 			"smart-door-lock-iot/log-activities",
 			2,
 			func(c mqtt.Client, m mqtt.Message) {
-				println("Log Activity: ", string(m.Payload()))
+				var logReq requests.CreateLogRequest
+				if err := json.Unmarshal(m.Payload(), &logReq); err != nil {
+					println("failed to unmarshal logReq activity: " + err.Error())
+				} else {
+					handlers.Log.CreateLog(logReq)
+				}
 			},
 		); token.WaitTimeout(time.Second*5) && token.Error() != nil {
 			panic("failed to subscribe to log activities topic: " + token.Error().Error())
