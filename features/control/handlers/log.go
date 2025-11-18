3f47d5f71e136fdd2a5c864ddc8043852ed855c9
Rizal Anggoro <gnoogler4@gmail.com>
2025-11-18 15:30:59 +0700

    feat: add GetAllLogs and DeleteAllLogs API endpoints with response definitions

--- features/control/handlers/log.go
+++ features/control/handlers/log.go
@@ -29,6 +29,10 @@ func (h *Log) CreateLog(req requests.CreateLogRequest) {
 	)
 }
 
+// @id 			GetAllLogs
+// @tags 		Control
+// @success 	200 {object} responses.GetAllLogsResponse
+// @router 		/api/v1/control/logs [get]
 func (h *Log) GetAllLogs(c *gin.Context) {
 	if res, err := h.service.GetAllLogs(); err != nil {
 		c.AbortWithStatusJSON(
@@ -41,6 +45,10 @@ func (h *Log) GetAllLogs(c *gin.Context) {
 	}
 }
 
+// @id 			DeleteAllLogs
+// @tags 		Control
+// @success 	200 {object} responses.DeleteAllLogsResponse
+// @router 		/api/v1/control/logs [delete]
 func (h *Log) DeleteAllLogs(c *gin.Context) {
 	if res, err := h.service.GetAllLogs(); err != nil {
 		c.AbortWithStatusJSON(
